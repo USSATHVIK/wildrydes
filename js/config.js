bb278236bd7dca4b7c808cf20e98da2ba412f454
Sathvik U S <125984065+USSATHVIK@users.noreply.github.com>
2025-05-06 01:31:54 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_9MpoF6By9', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'ap-south-1:f93ee6af-0eec-482b-84f2-b3af1f76f5b8', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '2gor63dh2hlpkqhsfdtncj74gt', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
